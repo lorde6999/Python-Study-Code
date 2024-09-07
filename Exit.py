0304c9f5792b0604459ba268f58809a13c9e1f81
Mateus Zamorano <52588123+lorde6999@users.noreply.github.com>
2024-09-07 17:22:51 -0300

    Exit.py

---
+++ Exit.py
@@ -0,0 +1,35 @@
+e1 = int(input("Digite apenas a HORA de entrada: "))
+e2 = int(input("Digite apenas os MINUTOS de entrada: "))
+e3 = int(input("Digite apenas a HORA de entrada: "))
+e4 = int(input("Digite apenas os MINUTOS de entrada: "))
+
+resul1 = e1 + e3
+resul2 = e2 + e4
+
+if resul2 >= 60: 
+    resul1 += 1
+    resul2 -= 60
+else:
+    pass
+
+if resul1 <= 12:
+    print(f"O horário de saída foi: {resul1}:{resul2}")
+
+elif resul1 > 12 and resul1 < 24:
+    resul1 -= 12
+    print(f"O horário de saída foi: {resul1}:{resul2}")
+
+elif resul1 >= 24 and resul1 < 36:
+    resul1 -= 24
+    print(f"O horário de saída foi: {resul1}:{resul2}")
+
+elif resul1 >= 36 and resul1 <= 48:
+    resul1 -= 36
+    print(f"O horário de saída foi: {resul1}:{resul2}")
+
+elif resul1 >= 48:
+    resul1 -= 48
+    print(f"O horário de saída foi: {resul1}:{resul2}")
+
+else:
+    pass
