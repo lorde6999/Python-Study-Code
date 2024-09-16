90bdaaa0dc487a564133a513f81daa325c1285a8
Mateus Zamorano <52588123+mateuszamorano@users.noreply.github.com>
2024-09-16 11:21:22 -0300

    Exercício24.py

--- Exercício24.py
+++ Exercício24.py
@@ -1,19 +1,4 @@
-z = 1
-for n in range (1, z+1):
-    print (z, end = " ")
-
-x = 2
-for n in range (1, x+1):
-    print (x, end = " ")
-
-c = 3
-for n in range (1, c+1):
-    print (c, end = " ")
-
-v = 4
-for n in range (1, v+1):
-    print (v, end = " ")
-
-b = 5
-for n in range (1, b+1):
-    print (b, end = " ")
+num = int(input("Digite um número: "))
+for n in range (1, num+1):
+    for x in range(1, n+1):
+        print (n, end = " ")
